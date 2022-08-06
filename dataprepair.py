9853a9365938fa7b3aa6f9a70342221846600c0d
Tanapattara Wongkhamchan <tanapattara@kku.ac.th>
2022-08-06 13:30:30 +0700

    add link and category improve code improve scolling

--- dataprepair.py
+++ dataprepair.py
@@ -12,7 +12,7 @@ from alive_progress import alive_bar
 from os import listdir
 from os.path import isfile, join
 
-def addUser(df, username):
+def addUser(df, username, link, review):
     key = 1
     path_to_file = "data/user.csv"
     if len(df) > 0:
@@ -20,13 +20,13 @@ def addUser(df, username):
         if username not in df.values:           
             # add username data in df 
             key = len(df) + 1  
-            newuser = pd.DataFrame([[key, username]], columns=['userid', 'name'])    
+            newuser = pd.DataFrame([[key, username, link, review]], columns=['userid', 'name', 'link', 'review'])    
             df = pd.concat([df, newuser], ignore_index = True)
         else:
             row = df.loc[df['name'] == username]
             key = int(row['userid'])
     else:
-        newuser = pd.DataFrame([[key, username]], columns=['userid', 'name']) 
+        newuser = pd.DataFrame([[key, username, link, review]], columns=['userid', 'name', 'link', 'review'])  
         df = pd.concat([df, newuser], ignore_index = True)    
     return key, df
 
@@ -34,21 +34,33 @@ def addVenue(df, venueName):
     key = 1
     #remove filetype
     venueName = venueName.split('.')[0]
+    link, category = getVenueLink(venueName)
     path_to_file = "data/venue.csv"
     if len(df) > 0:
         # have data in df
         if venueName not in df.values:           
             # add username data in df
             key = len(df) + 1
-            newVenue = pd.DataFrame([[key, venueName]], columns=['venueid', 'name'])
+            newVenue = pd.DataFrame([[key, venueName, category, link]], columns=['venueid', 'name', 'category', 'link'])
             df = pd.concat([df, newVenue], ignore_index = True)
         else:
             row = df.loc[df['name'] == venuename]
             key = int(row['venueid'])        
     else:
-        newVenue = pd.DataFrame([[key, venueName]], columns=['venueid', 'name'])
+        newVenue = pd.DataFrame([[key, venueName, category, link]], columns=['venueid', 'name', 'category', 'link'])
         df = pd.concat([df, newVenue], ignore_index = True) 
     return key, df
+
+def getVenueLink(venueName):
+    df = pd.read_csv('results/place.csv', sep='|')
+    link = ""
+    for index, row in df.iterrows():
+        if venueName in row['name']:
+            link = row.link
+            category = row.category
+            break    
+    return link, category
+
 
 mypath = "results/csv/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -68,12 +80,15 @@ for venuename in onlyfiles:
     titlebar = f'loading venue {i}/{len(onlyfiles)}'
     with alive_bar(len(place), title=titlebar) as bar:        
         for index, row in place.iterrows():
-            username = row['name'].strip()
-            userid, userdf = addUser(userdf, username)         
+            username = row['name'].strip()      
 
             score = float(row['score'])
             time = float(row['time'])
             comment = row['comment']
+            link = row['link']
+            review = row['review']
+
+            userid, userdf = addUser(userdf, username, link, review)  
             #add id to ratingdf name,score,time,comment
             newRating = pd.DataFrame([[venueid, userid, score, time, comment]], columns=['venueid', 'userid', 'score', 'time', 'comment'])
             ratingdf = pd.concat([ratingdf, newRating], ignore_index = True)
--- getgooglereview.py
+++ getgooglereview.py
@@ -1,3 +1,4 @@
+from tkinter import X
 from warnings import catch_warnings
 from numpy import double
 from selenium import webdriver
@@ -19,7 +20,7 @@ def scrollandload(driver):
     all_review_score = soup.find_all('div',{'class':'fontBodySmall'})
     allcomment = int(all_review_score[0].text.split()[0].replace(',', ''))
     loaded = 0
-    n = 5
+    n = 10
     count = 0
     while loaded < allcomment:        
         i = 0
@@ -38,10 +39,10 @@ def scrollandload(driver):
         
         loaded = len(currentload)
 
-        if count == 2:
+        if count == 3:
             break
         
-        n *= 2            
+        n += 10            
 
 def convertTime(ttext):
 
@@ -76,23 +77,45 @@ def loaddata(driver):
     list_time = []
     list_comment = []
     list_link = []
-    list_img = []
+    list_review = []
 
     with alive_bar(len(all_review_score)) as bar:
-        for x in all_review_score:
-            name = x['aria-label']
-            user = x.findChildren('a', {'class':'WEBjve'})
-            us = list(user)[0]
-            user_link = us['href']
-            user_img = us.img['src']
+        for review_element in all_review_score:
+            name = review_element['aria-label']
 
-            profiles = x.findChildren('div', {'class':'DU9Pgb'})
+            user_link = review_element.findChildren('a', {'class':'WEBjve'})[0]['href']
+            
+            user_review = review_element.findChildren('div', {'class':'RfnDt'})
+
+            user_review_n = 0
+
+            if len(user_review) > 0:
+                user_review = review_element.findChildren('div', {'class':'RfnDt'})[0]
+
+                txt = ''
+                try:            
+                    if len(list(user_review.children)) == 1:
+                        txt = list(user_review.children)[0].text
+                    elif len(list(user_review.children)) == 2:
+                        txt = list(user_review.children)[1].text
+
+                    if '·' in txt:
+                        txt = txt.replace('·', '').strip()
+                    if bool(txt.strip()):
+                        txt = txt.split(' ')[0]
+                        if ',' in txt:
+                            txt = txt.replace(',','').strip()
+                        user_review_n = int(txt)
+                except:
+                    print(txt)
+            
+            profiles = review_element.findChildren('div', {'class':'DU9Pgb'})
             pf = list(profiles)[0]
             score = pf.findChildren('span', {'class':'kvMYJc'})[0]
             score = score['aria-label'].split()[0]
             stime = pf.findChildren('span', {'class':'rsqaWe'})[0]
             stime = convertTime(stime.text)
-            comment = x.findChildren('span', {'class':'wiI7pd'})
+            comment = review_element.findChildren('span', {'class':'wiI7pd'})
             comment = comment[0].text
 
             list_name.append(name)
@@ -100,36 +123,40 @@ def loaddata(driver):
             list_time.append(stime)
             list_comment.append(comment)
             list_link.append(user_link)
-            list_img.append(user_img)
+            list_review.append(user_review_n)
             bar()
             
 
-    df = pd.DataFrame([list_name, list_score, list_time, list_comment, list_link, list_img])
+    df = pd.DataFrame([list_name, list_score, list_time, list_comment, list_link, list_review])
     df = df.transpose()
-    df.columns = ['name', 'score', 'time', 'comment', 'link', 'img']
+    df.columns = ['name', 'score', 'time', 'comment', 'link', 'review']
     return df
 
-def saveplacedetail(driver, lat, lon):
+def saveplacedetail(driver, lat, lon, place_url):
     #get name of place
     data = driver.page_source
     soup = bs4.BeautifulSoup(data, "lxml")
     name_element = soup.find_all('h1',{'class':'DUwDvf fontHeadlineLarge'})
     place_name = name_element[0].text.strip()
     #get overall score of place
-    place_score = soup.find_all('div', {'class', 'F7nice mmu3tf'})
-    if(place_score[0].span.text.strip() != ''):
-        place_score = float(place_score[0].span.text)
-    else:
+    p_score = soup.find_all('div', {'class', 'F7nice mmu3tf'})
+
+    place_score = 0
+    try:
+        if(p_score[0].span.text.strip() != ''):
+            place_score = float(p_score[0].span.text)
+    except:
         place_score = 0
+            
 
     #get category
     place_detail = soup.find_all('div', {'class', 'skqShb'})
     place_category = place_detail[0].contents[1].text.replace('·','') if '·' in  place_detail[0].contents[1].text else place_detail[0].contents[1].text
     
     #add place data to dataframe
-    place_data = pd.DataFrame([place_name, place_score, place_category, lat, lon])
-    place_data = place_data.transpose()
-    place_data.columns = ['name','score','category','latitude','longitude']
+    place_data_df = pd.DataFrame([place_name, place_score, place_category, lat, lon, place_url])
+    place_data_df = place_data_df.transpose()
+    place_data_df.columns = ['name','score','category','latitude','longitude', 'link']
     path_to_file = 'results\place.csv'
     
     parent_dir = ""
@@ -140,22 +167,20 @@ def saveplacedetail(driver, lat, lon):
             os.makedirs(path, 0o666)
     except OSError:
         print('Fatal: output directory "' + path + '" does not exist and cannot be created')
-            
 
     #check exist data in place.csv file
-    existrecord = False
+    isExistRecord = False
     if os.path.exists(path_to_file):
-        existdata = pd.read_csv(path_to_file)
-        for data in existdata.values:
-            if place_name in data:
-                existrecord = True
+        existdata = pd.read_csv(path_to_file, sep='|')
+        for index, row in existdata.iterrows():
+            if place_name in row['name']:
+                isExistRecord = True
                 print(place_name + " is already exist.")
-        if not existrecord:
-            place_data = existdata.append(place_data, ignore_index = True)        
-    
-    #save to csv
-    place_data.to_csv(path_to_file, index=False, encoding='utf-8', sep='|')
-    return [existrecord, place_name]
+                break
+        if not isExistRecord:
+            place_data_df = pd.concat([existdata, place_data_df], ignore_index = True) 
+
+    return [isExistRecord, place_name, place_data_df]
 
 with open('data/placelist.csv', newline='', encoding='utf-8') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
@@ -186,37 +211,24 @@ with open('data/placelist.csv', newline='', encoding='utf-8') as csvfile:
         # wait for loadcontent
         time.sleep(3.0)
   
-        place_name = saveplacedetail(driver, lat, lon)
+        existrecord, place_name, place_data_df = saveplacedetail(driver, lat, lon, place_url)
 
-        if place_name[0]:
+        if existrecord:
             driver.close()
             continue
 
-        place_name = place_name[1]
         #get review button
-
         review_buttons = driver.find_elements(By.CLASS_NAME, 'HHrUdb')
         if len(review_buttons) > 0:
             last_element = review_buttons[-1]
             ActionChains(driver).click(last_element).perform()
             time.sleep(3.0)
             scrollandload(driver)
-            dataloaded = loaddata(driver)
+            data_loaded = loaddata(driver)
 
             parent_dir = "results"
 
-            dirxlsx= "xlsx"
-            dircsv = "csv"
-            
-            #save to excel
-            path = os.path.join(parent_dir, dirxlsx)
-            try:
-                if not os.path.exists(path):
-                    os.makedirs(path, 0o666)
-            except OSError:
-                print('Fatal: output directory "' + path + '" does not exist and cannot be created')
-            
-            dataloaded.to_excel('results/xlsx/' + place_name + '.xlsx', index=False, encoding='utf-8')
+            dircsv = "csv"       
 
             #save to csv
             path = os.path.join(parent_dir, dircsv)
@@ -225,11 +237,11 @@ with open('data/placelist.csv', newline='', encoding='utf-8') as csvfile:
                     os.makedirs(path, 0o666)
             except OSError:
                 print('Fatal: output directory "' + path + '" does not exist and cannot be created')
-            dataloaded.to_csv('results/csv/' + place_name + '.csv', index=False, encoding='utf-8', sep='|')
+            data_loaded.to_csv('results/csv/' + place_name + '.csv', index=False, encoding='utf-8', sep='|')
+
+            place_data_df.to_csv('results/place.csv', index=False, encoding='utf-8', sep='|')    
         else:
             print('can\'t find review button')            
             continue
 
         driver.close()
-        break
-
