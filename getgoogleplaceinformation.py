6add6a1432cc2e6f4f70fec9037bc3ebe851a1d4
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2022-09-21 00:51:37 +0700

    add try catch exception

--- getgoogleplaceinformation.py
+++ getgoogleplaceinformation.py
@@ -28,29 +28,41 @@ ulink = "https://www.google.com/maps"
 # load venue
 venuedf = pd.read_csv("data/venue_metadata.csv", sep='|', encoding='utf-8')
 categorydf = pd.DataFrame()
-for i, row in venuedf.iterrows():
-    venue_name = row['name']
-    venue_id = row['venueid']
-    # open web
-    chrome_options = Options()
-    chrome_options.add_argument("--dns-prefetch-disable")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+with alive_bar(len(venuedf)) as bar:  
+    for i, row in venuedf.iterrows():
+        venue_name = row['name']
+        venue_id = row['venueid']
+        # open web
+        chrome_options = Options()
+        chrome_options.add_argument("--dns-prefetch-disable")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(ulink)
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(ulink)
 
-    inputElement = driver.find_element(By.ID, "searchboxinput")
-    inputElement.send_keys(venue_name)
-    buttonElement = driver.find_element(By.ID, "searchbox-searchbutton")
-    buttonElement.click()
-    time.sleep(5)
-    category = driver.find_element(By.CSS_SELECTOR, '.DkEaL.u6ijk')
-    strCategory = category.text
+        inputElement = driver.find_element(By.ID, "searchboxinput")
+        inputElement.send_keys(venue_name)
+        buttonElement = driver.find_element(By.ID, "searchbox-searchbutton")
+        buttonElement.click()
+        time.sleep(3)
+        try:
+            category = driver.find_element(By.CSS_SELECTOR, '.DkEaL.u6ijk')
+        except NoSuchElementException:
+            # have many place
+            # select top list
+            listitem = driver.find_element(By.CLASS_NAME, 'hfpxzc')
+            listitem.click()
+            time.sleep(1)
+            # load again
+            category = driver.find_element(By.CSS_SELECTOR, '.DkEaL.u6ijk')
 
-    if len(categorydf) > 0:
-        categorydf.loc[len(categorydf.index)] = [venue_id, strCategory]
-    else:
-        categorydf = pd.DataFrame([[venue_id, strCategory]], columns=['venueid', 'category'])
-    driver.close()
+        strCategory = category.text
+
+        if len(categorydf) > 0:
+            categorydf.loc[len(categorydf.index)] = [venue_id, strCategory]
+        else:
+            categorydf = pd.DataFrame([[venue_id, strCategory]], columns=['venueid', 'category'])
+        driver.close()
+        bar()
 
 categorydf.to_csv('data/categorydf.csv', sep='|', encoding='utf-8',index=False)
