2b6f95a3bd7b4854dc32c23979d450244eb9a137
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2022-09-26 22:42:14 +0700

    update googleplay category

--- getgoogleplaceinformation.py
+++ getgoogleplaceinformation.py
@@ -1,6 +1,7 @@
 """
 
 """
+from asyncio import constants
 from json import load
 from tkinter import X
 from warnings import catch_warnings
@@ -27,42 +28,63 @@ import os
 ulink = "https://www.google.com/maps"
 # load venue
 venuedf = pd.read_csv("data/venue_metadata.csv", sep='|', encoding='utf-8')
+
 categorydf = pd.DataFrame()
+badcategorydf = pd.DataFrame()
+
+path_to_file = 'data/categorydf.csv'
+if os.path.exists(path_to_file):
+    categorydf = pd.read_csv("data/categorydf.csv", sep='|', encoding='utf-8')
+path_to_file = 'data/badcategorydf.csv'
+if os.path.exists(path_to_file):
+    badcategorydf = pd.read_csv("data/badcategorydf.csv", sep='|', encoding='utf-8')
+
+
 with alive_bar(len(venuedf)) as bar:  
     for i, row in venuedf.iterrows():
         venue_name = row['name']
         venue_id = row['venueid']
-        # open web
-        chrome_options = Options()
-        chrome_options.add_argument("--dns-prefetch-disable")
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(ulink)
-
-        inputElement = driver.find_element(By.ID, "searchboxinput")
-        inputElement.send_keys(venue_name)
-        buttonElement = driver.find_element(By.ID, "searchbox-searchbutton")
-        buttonElement.click()
-        time.sleep(3)
+        bar()
+        if venue_id in categorydf.values :
+            continue
+        if venue_id in badcategorydf.values :
+            continue        
         try:
-            category = driver.find_element(By.CSS_SELECTOR, '.DkEaL.u6ijk')
-        except NoSuchElementException:
-            # have many place
-            # select top list
-            listitem = driver.find_element(By.CLASS_NAME, 'hfpxzc')
-            listitem.click()
-            time.sleep(1)
-            # load again
-            category = driver.find_element(By.CSS_SELECTOR, '.DkEaL.u6ijk')
+            # open web
+            chrome_options = Options()
+            chrome_options.add_argument("--dns-prefetch-disable")
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-        strCategory = category.text
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.get(ulink)
+
+            inputElement = driver.find_element(By.ID, "searchboxinput")
+            inputElement.send_keys(venue_name)
+            buttonElement = driver.find_element(By.ID, "searchbox-searchbutton")
+            buttonElement.click()
+            time.sleep(5)
+            strCategory = ""
+     
+            category = driver.find_element(By.CSS_SELECTOR, '.DkEaL.u6ijk')
+            strCategory = category.text
+        except:
+            if len(badcategorydf) > 0:
+                badcategorydf.loc[len(badcategorydf.index)] = [venue_id, venue_name]
+            else:
+                badcategorydf = pd.DataFrame([[venue_id, venue_name]], columns=['venueid', 'name'])
+            
+            categorydf.to_csv('data/categorydf.csv', sep='|', encoding='utf-8',index=False)
+            badcategorydf.to_csv('data/badcategorydf.csv', sep='|', encoding='utf-8',index=False)
+ 
+        driver.close()
+        if strCategory == "":
+            continue
 
         if len(categorydf) > 0:
-            categorydf.loc[len(categorydf.index)] = [venue_id, strCategory]
+            categorydf.loc[len(categorydf.index)] = [venue_id, venue_name, strCategory]
         else:
-            categorydf = pd.DataFrame([[venue_id, strCategory]], columns=['venueid', 'category'])
-        driver.close()
-        bar()
+            categorydf = pd.DataFrame([[venue_id, venue_name, strCategory]], columns=['venueid','name', 'category'])        
+        
 
 categorydf.to_csv('data/categorydf.csv', sep='|', encoding='utf-8',index=False)
+badcategorydf.to_csv('data/badcategorydf.csv', sep='|', encoding='utf-8',index=False)
