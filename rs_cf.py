8bfe0b3e6499cb56eb0a33efab66e67acd9284ad
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2022-10-20 00:44:33 +0700

    update

--- rs_cf.py
+++ rs_cf.py
@@ -1,6 +1,7 @@
 # https://realpython.com/build-recommendation-engine-collaborative-filtering/
 from cgi import test
 import math
+from tokenize import Double
 from unittest import result
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
@@ -56,7 +57,9 @@ def simuv(uid:int, vid:int, rdf, simdf):
         vrating = row['score'] - umean
         vpow += vrating * vrating
     
-    simvalue = sum / math.sqrt(upow) * math.sqrt(vpow)
+    simvalue = 0
+    if  upow != 0 and vpow != 0:
+        simvalue = sum / math.sqrt(upow) * math.sqrt(vpow)
 
     if len(simdf) > 0:
         simdf.loc[len(simdf.index)] = [uid, vid, simvalue]
@@ -65,9 +68,28 @@ def simuv(uid:int, vid:int, rdf, simdf):
 
     return simdf
 
+def getfile(filepath):
+    df = pd.DataFrame()
+    try:
+        if os.path.exists(filepath):
+            df = pd.read_csv(filepath, sep='|')
+    except:
+        return df
+    return df
+
+def add2predict(u,v,score, pscore,df):
+    if len(df) > 0:
+        df.loc[len(df.index)] = [u, v, score, pscore]
+        df.to_csv('data/predictdf.csv', sep='|', encoding='utf-8',index=False)
+    else:
+        df = pd.DataFrame([[u, v, score, pscore]], columns=['u', 'v', 's','p'])
+
+    return df
+
 # train-test data
 testdf = pd.DataFrame()
 traindf = pd.DataFrame()
+
 filepath = 'data/testdf.csv'
 if os.path.exists(filepath):
     testdf = pd.read_csv("data/testdf.csv", sep='|')
@@ -76,32 +98,63 @@ else:
     testdf = ratingsdf.sample(frac=0.1, replace=True, random_state=1)
     traindf = pd.concat([ratingsdf,testdf]).drop_duplicates(keep=False)    
 
+predictdf = getfile('data/predictdf.csv')
+
 testdf.to_csv('data/testdf.csv', sep='|', encoding='utf-8',index=False)
 traindf.to_csv('data/traindf.csv', sep='|', encoding='utf-8',index=False)
 
-simdf = pd.DataFrame()
-filepath = 'data/simdf.csv'
-if os.path.exists(filepath):
-    simdf = pd.read_csv("data/simdf.csv", sep='|')
-i = 1
+simdf = getfile('data/simdf.csv')
+
+testi = 0
 for index, row in testdf.iterrows():
-    uid = int(row['userid'])
-    # get userdf
-    udf = traindf.loc[traindf['userid'] == uid]
-    # loop all userdf
-    ui = 1
-    for useridex, urow in udf.iterrows():
-        venueid = urow['venueid']
-        # venue df that show all usercheckin
-        venuedf = traindf.loc[traindf['venueid'] == venueid]
-        titlebar = f'loading {i}/{len(testdf)} at user {ui}/{len(udf)}'
-        with alive_bar(len(venuedf), title=titlebar) as bar:  
-            for vindex, vrow in venuedf.iterrows():
-                vid = int(vrow['userid'])
-                if uid == vid:
-                    continue
-                simdf = simuv(uid, vid, rdf=testdf, simdf=simdf)
-                bar()
+    target_useru_id = int(row['userid'])
+    target_venue_id = int(row['venueid'])
+    target_score = row['score']
+
+    if len(predictdf) > 0 and ( target_useru_id in predictdf['u'] and target_venue_id in predictdf['v']):
+        continue
+
+    target_venue_user_rating_df = traindf.loc[traindf['venueid'] == target_venue_id]
+    if len(target_venue_user_rating_df) <= 1:
+        continue
+
+    # loop all user that rating at target venue
+    with alive_bar(len(target_venue_user_rating_df), title=f'working {testi}/{len(testdf)}') as bar:
+        for useridex, user_rating_row in target_venue_user_rating_df.iterrows():
+            bar()
+            user_v_id = int(user_rating_row['userid'])
+            if target_useru_id == user_v_id:
+                continue
+            
+            # venue df that show all usercheckin
+            user_v_checkin_df = traindf.loc[traindf['userid'] == user_v_id]
+            simdf = simuv(target_useru_id, user_v_id, rdf=testdf, simdf=simdf)
             simdf.to_csv('data/simdf.csv', sep='|', encoding='utf-8',index=False)
-        ui+=1
-    i+=1
+    
+    # check top similarity
+    bestsimdf = simdf.loc[simdf['u'] == target_useru_id]
+    bestsimdf = bestsimdf.sort_values(by=['s'], ascending=False)
+    bestsimdf = bestsimdf.loc[bestsimdf['s'] > 0.5]
+    
+    zsim = 0
+    zscore = 0
+    pred_score_u_2_target = -1
+    if len(bestsimdf) > 0:
+        for simindex, simrow in bestsimdf.iterrows():
+            vid = int(simrow['v'])
+            if vid == target_useru_id:
+                continue
+
+            bestsim = int(simrow['s'])
+            train_v_df = traindf.loc[(traindf['userid'] == vid) & (traindf['venueid'] == target_venue_id)]
+
+            if len(train_v_df) == 0:
+                continue
+
+            zsim += bestsim
+            zscore += bestsim * int(train_v_df['score'])
+        if zsim != 0:
+            pred_score_u_2_target = zscore / zsim
+    
+    predictdf = add2predict(target_useru_id,target_venue_id, target_score, pred_score_u_2_target, predictdf)
+    testi += 1
