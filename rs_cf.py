cde94954cc5dc745defcc092676afc77e968f25c
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2022-10-29 09:35:24 +0700

    update sim

--- rs_cf.py
+++ rs_cf.py
@@ -1,7 +1,10 @@
 # https://realpython.com/build-recommendation-engine-collaborative-filtering/
 from cgi import test
+from curses.ascii import islower
 import math
+from pickle import TRUE
 import re
+from tkinter.tix import COLUMN
 from tokenize import Double
 from unittest import result
 from scipy.sparse import csr_matrix
@@ -42,6 +45,12 @@ def simuv(uid: int, vid: int, rdf, simdf):
 
     udf = rdf.loc[rdf['userid'] == uid]
     vdf = rdf.loc[rdf['userid'] == vid]
+
+    # udf = udf.drop(columns=['comment', 'Exist'])
+    # vdf = vdf.drop(columns=['comment', 'Exist'])
+    # udf.to_csv('data/udf.csv', sep='|', encoding='utf-8', index=False)
+    # vdf.to_csv('data/vdf.csv', sep='|', encoding='utf-8', index=False)
+
     umean = udf['score'].mean()
     vmean = vdf['score'].mean()
 
@@ -53,40 +62,45 @@ def simuv(uid: int, vid: int, rdf, simdf):
     u_pow = 0
     v_pow = 0
 
-    for i, row in udf.iterrows():
-        u_rating = row['score']
-        um_rating = u_rating - umean
-        um_pow += pow(um_rating, 2)
-        u_pow += pow(u_pow, 2)
+    sim_m_value = 0
+    sim_value = 0
 
-        venueid = row['venueid']
-        same_score_df = vdf.loc[vdf['venueid'] == venueid]
+    mergedf = udf.merge(vdf, how='inner', on='venueid')
 
-        v_rating = 0
-        if len(same_score_df) == 1:
-            v_rating = float(same_score_df['score'])
-        elif len(same_score_df) > 1:
-            v_rating = float(same_score_df['score'].mean())
+    if len(mergedf) > 0:
+        for i, row in udf.iterrows():
+            u_rating = float(row['score'])
+            um_rating = u_rating - umean
+            um_pow += pow(um_rating, 2)
+            u_pow += pow(u_rating, 2)
 
-        if v_rating != 0:
+            venueid = int(row['venueid'])
+            same_score_df = vdf.loc[vdf['venueid'] == venueid]
+
+            if len(same_score_df) == 0:
+                continue
+
+            v_rating = 0
+            if len(same_score_df) == 1:
+                v_rating = float(same_score_df['score'])
+            elif len(same_score_df) > 1:
+                v_rating = float(same_score_df['score'].mean())
+
             vm_rating = v_rating - vmean
             sum += u_rating * v_rating
             summ += um_rating * vm_rating
 
-    if summ != 0:
         for i, row in vdf.iterrows():
-            v_rating = row['score']
+            v_rating = float(row['score'])
             vm_rating = v_rating - vmean
             vm_pow += pow(vm_rating, 2)
             v_pow += pow(v_rating, 2)
 
-    sim_m_value = 0
-    if um_pow != 0 and vm_pow != 0:
-        sim_m_value = summ / math.sqrt(um_pow) * math.sqrt(vm_pow)
+        if um_pow != 0 and vm_pow != 0:
+            sim_m_value = summ / (math.sqrt(um_pow) * math.sqrt(vm_pow))
 
-    sim_value = 0
-    if u_pow != 0 and v_pow != 0:
-        sim_value = sum / math.sqrt(u_pow) * math.sqrt(v_pow)
+        if u_pow != 0 and v_pow != 0:
+            sim_value = sum / (math.sqrt(u_pow) * math.sqrt(v_pow))
 
     if len(simdf) > 0:
         simdf.loc[len(simdf.index)] = [uid, vid, sim_value, sim_m_value]
@@ -97,8 +111,13 @@ def simuv(uid: int, vid: int, rdf, simdf):
     return simdf
 
 
-def getfile(filepath):
+def getfile(filepath, isLoad=True):
+
     df = pd.DataFrame()
+
+    if not isLoad:
+        return df
+
     try:
         if os.path.exists(filepath):
             df = pd.read_csv(filepath, sep='|')
@@ -127,6 +146,7 @@ def pred_cf(uid, vid, uvscore, simdf, predictdf):
 
     zsim = 0
     zscore = 0
+
     pred_score_u_2_mean_target = -1
     if len(best_sim_m_df) > 0:
         for simindex, simrow in best_sim_m_df.iterrows():
@@ -181,6 +201,7 @@ def pred_cf(uid, vid, uvscore, simdf, predictdf):
     venuedf = pd.read_csv('data/venue_metadata.csv', sep='|')
     row = venuedf.loc[venuedf['venueid'] == vid]
     popScore = -1
+
     if (len(row) > 0):
         popScore = float(row['score'])
 
@@ -201,12 +222,13 @@ else:
     testdf = ratingsdf.sample(frac=0.1, replace=True, random_state=1)
     traindf = pd.concat([ratingsdf, testdf]).drop_duplicates(keep=False)
 
-predictdf = getfile('data/predictdf.csv')
 
 testdf.to_csv('data/testdf.csv', sep='|', encoding='utf-8', index=False)
 traindf.to_csv('data/traindf.csv', sep='|', encoding='utf-8', index=False)
 
-simdf = getfile('data/simdf.csv')
+isLoad = False
+simdf = getfile('data/simdf.csv', isLoad)
+predictdf = getfile('data/predictdf.csv', isLoad)
 
 testi = 0
 for index, row in testdf.iterrows():
