74dee4b8d1ed31a613c7d3e99fa1fb5235043996
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2023-06-18 18:34:53 +0700

    update element and driver

--- README.md
+++ README.md
@@ -9,19 +9,19 @@
 ที่ Terminal เรียกใช้คำสั่ง pip install ตามด้วย package เพื่อทำการติดตั้ง
 
 ```
-pip install selenium pandas lxml openpyxl bs4 numpy 
+pip install selenium pandas lxml openpyxl bs4 numpy alive_progress
 ```
 
 ### การทำงาน
 
 1. ใส่ url ของสถานที่ที่ได้จาก Google map `data/placelist.csv` โดยสถานที่แต่ละที่ ต่อหนึ่งบรรทัด
-2. สั่งทำงานไฟล์ `getgooglereview.py`
+2. สั่งทำงานไฟล์ `googleplace.py`
    ```
-   python getgooglereview.py
+   python googleplace.py
    ```
 3. ข้อมูลที่รวบรวมได้จะถูกเก็บไว้ในโฟลเดอร์ `results` แยกเป็น 2 ส่วน
    1. ข้อมูลสถานที่จะถูกเก็บไว้ที่ `results/place.csv`
-   2. ข้อมูลรีวิวจะถูกเก็บไว้ที่ `results/placeid.csv`
+   2. ข้อมูลรีวิวจะถูกเก็บไว้ที่ `results/csv/PLACE_NAME.csv`
 
 ## Dataframe diagram
 
@@ -46,3 +46,13 @@ pip install selenium pandas lxml openpyxl bs4 numpy
 | 1        | 1      | 5     | 0.1  | comment text |
 
 _หน่วยของ time เป็นปี หากน้อยกว่า 1 ปี จะใช้จุดทศนิยม เช่น 1 เดือนใช้ 0.1 เป็นต้น แต่หากมีระยะเวลานานกว่า 10 เดือนขึ้นไปนับเป็น 1 ปี_
+
+### ChromeDriver ERROR
+
+กรณีเกิดปัญหาข้อผิดพลาดในการเริ่มการทำงาน แสดงข้อผิดพลาดกรณี chromedriver.exe ไม่ตรงกับเวอร์ชั่นของ Chrome ในเครื่องคอมพิวเตอร์ ให้ทำการดาวโหลด chromedriver.exe ตัวใหม่ได้ที่ [ChromeDriver](https://chromedriver.chromium.org/downloads) ทำการเลือกเวอร์ชั่นและระบบปฏิบัติการ
+
+```
+Exception has occurred: SessionNotCreatedException
+Message: session not created: This version of ChromeDriver only supports Chrome version 104
+Current browser version is 114.0.5735.134
+```
---
+++ googleplace.py
@@ -0,0 +1,281 @@
+"""
+For load placelist.csv
+
+return result of place.csv in results/csv folder
+"""
+from tkinter import X
+from warnings import catch_warnings
+from numpy import double
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from alive_progress import alive_bar
+
+import bs4
+import pandas as pd
+import time
+import csv
+import os.path
+import os
+
+
+def scrollandload(driver):
+    data = driver.page_source
+    soup = bs4.BeautifulSoup(data, "lxml")
+    all_review_score = soup.find_all('div', {'class': 'fontBodySmall'})
+    allcomment = int(all_review_score[0].text.split()[0].replace(',', ''))
+    loaded = 0
+    n = 10
+    count = 0
+    while loaded < allcomment:
+        i = 0
+
+        while i < n:
+            html = driver.find_elements(By.CLASS_NAME, 'DxyBCb')
+            html[0].send_keys(Keys.PAGE_DOWN)
+            i += 1
+
+        data = driver.page_source
+        soup = bs4.BeautifulSoup(data, "lxml")
+        currentload = soup.find_all('div', {'class': 'jftiEf fontBodyMedium'})
+
+        if loaded == len(currentload):
+            count += 1
+
+        loaded = len(currentload)
+
+        if count == 3:
+            break
+
+        n += 10
+
+
+def convertTime(ttext):
+
+    if "เดือน" in ttext:
+        tsplited = ttext.split()
+        try:
+            tsplited = int(tsplited[0])
+        except ValueError:
+            tsplited = 1
+
+        if tsplited > 9:
+            return double(1)
+        else:
+            tsplited = tsplited / 10
+            return double(tsplited)
+    elif "ปี" in ttext:
+        tsplited = ttext.split()
+        if len(tsplited) > 1:
+            return double(tsplited[0])
+        else:
+            return 1.0
+    else:
+        return 0.1
+
+
+def loaddata(driver):
+
+    # get more review button
+    more_review_btn_elements = driver.find_elements(
+        By.CSS_SELECTOR, "div.MyEned")
+    for more_btn in more_review_btn_elements:
+        try:
+            more_review_btn_element = more_btn.find_element(
+                By.CSS_SELECTOR, "button.w8nwRe.kyuRq")
+            if more_review_btn_element:
+                ActionChains(driver).click(more_review_btn_element).perform()
+        except Exception as e:
+            print("no button to click")
+
+    data = driver.page_source
+    soup = bs4.BeautifulSoup(data, "lxml")
+    all_review_score = soup.find_all('div', {'class': 'jftiEf fontBodyMedium'})
+
+    list_name = []
+    list_score = []
+    list_time = []
+    list_comment = []
+    list_link = []
+    list_review = []
+
+    with alive_bar(len(all_review_score)) as bar:
+        for review_element in all_review_score:
+
+            name = review_element['aria-label']
+
+            user_link = review_element.find(
+                'button', {'class': 'al6Kxe'}).get('data-href')
+
+            user_review_n = 0
+            user_review = review_element.find(
+                'div', {'class': 'RfnDt'})
+            if user_review:
+                txt = user_review.text
+                try:
+                    # check if user review is number
+                    if 'รีวิว' in txt:
+                        txt = txt.replace('รีวิว', '').strip()
+                    if '·' in txt:
+                        txt = txt.replace('·', '').strip()
+                    if 'Local Guide' in txt:
+                        txt = txt.replace('Local Guide', '').strip()
+                    user_review_n = int(txt)
+                except:
+                    print(txt)
+
+            score = review_element.find('span', {'class': 'kvMYJc'})
+            score = int(score['aria-label'].split()[0])
+
+            stime = review_element.find('span', {'class': 'rsqaWe'}).text
+            stime = convertTime(stime)
+
+            # get comment
+            comment = ""
+            comment_element = review_element.find('span', {'class': 'wiI7pd'})
+            if comment_element:
+                comment = comment_element.text
+
+            list_name.append(name)
+            list_score.append(score)
+            list_time.append(stime)
+            list_comment.append(comment)
+            list_link.append(user_link)
+            list_review.append(user_review_n)
+            bar()
+
+    df = pd.DataFrame([list_name, list_score, list_time,
+                      list_comment, list_link, list_review])
+    df = df.transpose()
+    df.columns = ['name', 'score', 'time', 'comment', 'link', 'review']
+    return df
+
+
+def saveplacedetail(driver, lat, lon, place_url):
+    # get name of place
+    data = driver.page_source
+    soup = bs4.BeautifulSoup(data, "lxml")
+    name_element = soup.find_all('h1', {'class': 'DUwDvf fontHeadlineLarge'})
+    place_name = name_element[0].text.strip()
+    # get overall score of place
+    p_score = soup.find_all('div', {'class', 'F7nice mmu3tf'})
+
+    place_score = 0
+    try:
+        if (p_score[0].span.text.strip() != ''):
+            place_score = float(p_score[0].span.text)
+    except:
+        place_score = 0
+
+    # get category
+    place_detail = soup.find_all('div', {'class', 'skqShb'})
+    place_category = place_detail[0].contents[1].text.replace(
+        '·', '') if '·' in place_detail[0].contents[1].text else place_detail[0].contents[1].text
+
+    # add place data to dataframe
+    place_data_df = pd.DataFrame(
+        [place_name, place_score, place_category, lat, lon, place_url])
+    place_data_df = place_data_df.transpose()
+    place_data_df.columns = ['name', 'score',
+                             'category', 'latitude', 'longitude', 'link']
+    path_to_file = 'results\place.csv'
+
+    parent_dir = ""
+    dirxlsx = "results"
+    path = os.path.join(parent_dir, dirxlsx)
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path, 0o666)
+    except OSError:
+        print('Fatal: output directory "' + path +
+              '" does not exist and cannot be created')
+
+    # check exist data in place.csv file
+    isExistRecord = False
+    if os.path.exists(path_to_file):
+        existdata = pd.read_csv(path_to_file, sep='|')
+        for index, row in existdata.iterrows():
+            if place_name in row['name']:
+                isExistRecord = True
+                print(place_name + " is already exist.")
+                break
+        if not isExistRecord:
+            place_data_df = pd.concat(
+                [existdata, place_data_df], ignore_index=True)
+
+    return [isExistRecord, place_name, place_data_df]
+
+
+def main():
+    with open('data/placelist.csv', newline='', encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
+
+        for row in spamreader:
+            place_url = row[0]
+
+            # find location
+            surl = place_url.split('/')
+            locations = [x for x in surl if "@" in x]
+            if len(locations) > 1:
+                i = 0
+                for loc in locations:
+                    if len(loc.split(',')) == 3:
+                        break
+                    i += 1
+                locations = locations[i]
+            else:
+                locations = locations[0]
+
+            lat = float(locations.split(',')[0].replace('@', ''))
+            lon = float(locations.split(',')[1])
+
+            # open chrome
+            driver = webdriver.Chrome()
+            driver.get(place_url)
+
+            # wait for loadcontent
+            time.sleep(3.0)
+
+            existrecord, place_name, place_data_df = saveplacedetail(
+                driver, lat, lon, place_url)
+
+            if existrecord:
+                driver.close()
+                continue
+
+            # get review button
+            review_buttons = driver.find_elements(By.CLASS_NAME, 'HHrUdb')
+            if len(review_buttons) > 0:
+                last_element = review_buttons[-1]
+                ActionChains(driver).click(last_element).perform()
+                time.sleep(3.0)
+                scrollandload(driver)
+                data_loaded = loaddata(driver)
+
+                parent_dir = "results"
+
+                dircsv = "csv"
+
+                # save to csv
+                path = os.path.join(parent_dir, dircsv)
+                try:
+                    if not os.path.exists(path):
+                        os.makedirs(path, 0o666)
+                except OSError:
+                    print('Fatal: output directory "' + path +
+                          '" does not exist and cannot be created')
+                data_loaded.to_csv('results/csv/' + place_name +
+                                   '.csv', index=False, encoding='utf-8', sep='|')
+
+                place_data_df.to_csv('results/place.csv',
+                                     index=False, encoding='utf-8', sep='|')
+            else:
+                print('can\'t find review button')
+                continue
+
+            driver.close()
+
+
+if __name__ == "__main__":
+    main()
